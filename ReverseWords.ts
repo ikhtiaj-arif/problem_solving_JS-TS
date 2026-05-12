ee6a2e6768ed68fc8e27617fa50e86ea82a2d468
ikhtiaj-arif <ikhtiaj.arif@gmail.com>
2026-05-12 13:16:38 +0600

    reverse word

---
+++ ReverseWords.ts
@@ -0,0 +1,13 @@
+const str = "Hello World From Bangladesh";
+
+const reverseWord = (str: string): string => {
+  const strArr = str.split(" "); //* linear complexity O(n)
+  let resultArr = [];
+  for (let i = strArr.length - 1; i >= 0; i--) { //* linear complexity O(n)
+    resultArr.push(strArr[i]);
+  }
+  return resultArr.join(" "); //* linear complexity O(n)
+};
+
+console.log(reverseWord(str));
+//? overall linear complexity O(n)
