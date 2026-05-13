50e8ac2789923e334f649aa49dfd963ceb80a33a
ikhtiaj-arif <ikhtiaj.arif@gmail.com>
2026-05-13 19:17:45 +0600

    find vowels

---
+++ Vowels.ts
@@ -0,0 +1,19 @@
+const str = "JavaScript";
+
+const findVowel = (str: string) => {
+  const strArr = str.toLocaleLowerCase().split("");
+  let count: number = 0;
+  for (let i = 0; i < strArr.length; i++) {
+    if (
+      strArr[i] === "a" ||
+      strArr[i] === "e" ||
+      strArr[i] === "i" ||
+      strArr[i] === "o" ||
+      strArr[i] === "u"
+    ) {
+      count++;
+    }
+  }
+  return count
+};
+console.log(findVowel(str));
